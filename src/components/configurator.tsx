5c725cac62e7fd1aefcb90e61c23623facf6d5c2
peppapighs <ptk.prasertsuk@gmail.com>
2025-01-01 21:15:11 +0700

    :art: add tab switching

--- src/components/configurator.tsx
+++ src/components/configurator.tsx
@@ -1,6 +1,8 @@
 import { KeyboardDevice } from "@/types/keyboard-device"
+import { TabsContent } from "@radix-ui/react-tabs"
 import { Sidebar } from "./sidebar"
 import { SiteHeader } from "./site-header"
+import { Tabs } from "./ui/tabs"
 
 interface IConfiguratorProps {
   device: KeyboardDevice
@@ -8,14 +10,21 @@ interface IConfiguratorProps {
 
 export function Configurator({ device }: IConfiguratorProps) {
   return (
-    <div className="flex flex-1 flex-col divide-y">
-      <SiteHeader />
-      <main className="mx-auto grid w-full max-w-[1800px] grid-cols-[220px_minmax(0,1fr)] divide-x min-[1800px]:border-x">
-        <aside className="h-[calc(100vh-3.5rem-1px)] w-full">
-          <Sidebar device={device} />
-        </aside>
-        <div className="flex h-[calc(100vh-3.5rem-1px)] w-full flex-col"></div>
-      </main>
-    </div>
+    <Tabs defaultValue="remap" asChild>
+      <div className="flex flex-1 flex-col divide-y">
+        <SiteHeader />
+        <main className="mx-auto grid w-full max-w-[1800px] grid-cols-[220px_minmax(0,1fr)] divide-x min-[1800px]:border-x">
+          <aside className="h-[calc(100vh-3.5rem-1px)] w-full">
+            <Sidebar device={device} />
+          </aside>
+          <div className="flex h-[calc(100vh-3.5rem-1px)] w-full flex-col">
+            <TabsContent
+              value="remap"
+              className="flex max-h-full flex-1 flex-col"
+            ></TabsContent>
+          </div>
+        </main>
+      </div>
+    </Tabs>
   )
 }
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,13 +1,24 @@
 import Link from "next/link"
+import React from "react"
 import { ThemeSwitcher } from "./theme-switcher"
+import { TabsList, TabsTrigger } from "./ui/tabs"
 
 export function SiteHeader() {
   return (
     <header className="w-full bg-background">
-      <div className="mx-auto flex h-14 max-w-[1800px] items-center justify-between gap-6 px-6 min-[1800px]:border-x">
+      <div className="mx-auto flex h-14 max-w-[1800px] items-center gap-6 px-6 min-[1800px]:border-x">
         <Link href="/">
           <h1 className="text-xl font-extrabold tracking-tight">WEBHMK</h1>
         </Link>
+        <div className="flex flex-1 items-center justify-center">
+          <TabsList>
+            <TabsTrigger value="remap">Remap</TabsTrigger>
+            <TabsTrigger value="performance">Performance</TabsTrigger>
+            <TabsTrigger value="advanced">Advanced</TabsTrigger>
+            <TabsTrigger value="debug">Debug</TabsTrigger>
+            <TabsTrigger value="settings">Settings</TabsTrigger>
+          </TabsList>
+        </div>
         <ThemeSwitcher />
       </div>
     </header>
